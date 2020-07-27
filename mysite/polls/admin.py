9caf943a67f6ae30c5dbbb1f75e35cc8fd65816f
prd-nhut-le <nhut.le@paradox.ai>
2020-07-27 08:37:05 +0700

    part5 - unit test

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from django.contrib import admin
 
-from .models import Questions, Choice
+from .models import Question, Choice
 
-admin.site.register(Questions)
+admin.site.register(Question)
 admin.site.register(Choice)
